f0ec73b6d594616e766bd9cfa97aa1522efe3583
kimjungjin <jungks9351@gmail.com>
2022-09-16 03:03:54 +0900

    feat : 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MoviePage from './pages/MoviePage';
+
 function App() {
-  return <div>Hello TDD</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movie" element={<MoviePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/MoviePage.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/common/Header';
+
+const MoviePage = () => {
+  return (
+    <>
+      <Header />
+      <div>MoviePage</div>
+    </>
+  );
+};
+
+export default MoviePage;

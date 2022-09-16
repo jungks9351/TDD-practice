f3ef5b788f920295fdac4df3c0cda1a94a0d97e7
kimjungjin <jungks9351@gmail.com>
2022-09-16 14:28:57 +0900

    remove : 불필요 코드 제거

--- src/components/common/__test__/Header.test.tsx
+++ src/components/common/__test__/Header.test.tsx
@@ -1,5 +1,5 @@
 import { createMemoryHistory } from '@remix-run/router';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import Header from '../Header';
 
